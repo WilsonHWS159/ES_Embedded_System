a4865465d269aaefe342f189707e3787e1da1814
WilsonHWS159 <n96101189@gs.ncku.edu.tw>
2022-09-12 03:48:07 +0800

    Finish LAB08 requirment The blink_count function call in usart1_handler sometimes cause overrun

--- LAB08/2_usart_interrupt/main.c
+++ LAB08/2_usart_interrupt/main.c
@@ -1,5 +1,9 @@
 #include <stdint.h>
+#include "blink.h"
 #include "reg.h"
+
+const char *reset_msg = "Hello, world!\r\n";
+const char *ore_msg = "Overrun !!!\r\n";
 
 void init_usart1(void)
 {
@@ -44,6 +48,8 @@ void init_usart1(void)
 	WRITE_BITS(USART1_BASE + USART_BRR_OFFSET, DIV_FRACTION_3_BIT, DIV_FRACTION_0_BIT, DIV_FRACTION);
 	SET_BIT(USART1_BASE + USART_CR1_OFFSET, TE_BIT);
 	SET_BIT(USART1_BASE + USART_CR1_OFFSET, RE_BIT);
+	SET_BIT(USART1_BASE + USART_CR1_OFFSET, RXNEIE_BIT);
+	SET_BIT(NVIC_ISER_BASE + NVIC_ISERn_OFFSET(1), 5);
 }
 
 void usart1_send_char(const char ch)
@@ -54,28 +60,30 @@ void usart1_send_char(const char ch)
 
 char usart1_receive_char(void)
 {
-	for (; !READ_BIT(USART1_BASE + USART_SR_OFFSET, RXNE_BIT););
 	return (char) REG(USART1_BASE + USART_DR_OFFSET);
+}
+
+void usart1_handler()
+{
+	if (READ_BIT(USART1_BASE + USART_SR_OFFSET, ORE_BIT)) {
+		for (char *p = ore_msg; *p != '\0';)
+			usart1_send_char(*p++);
+		blink_count(LED_RED, 1);
+	} else if (READ_BIT(USART1_BASE + USART_SR_OFFSET, RXNE_BIT)) {
+		char ch = usart1_receive_char();
+		if (ch == '\r')
+			usart1_send_char('\n');
+		usart1_send_char(ch);
+		blink_count(LED_BLUE, 1);
+	}
 }
 
 int main(void)
 {
 	init_usart1();
 
-	char *hello = "Hello world!\r\n";
-
 	//send Hello world
-	for (char *p = hello; *p != '\0';)
+	for (char *p = reset_msg; *p != '\0';)
 		usart1_send_char(*p++);
-
-
-	//receive char and resend it
-	char ch;
-	for (;;)
-	{
-		ch = usart1_receive_char();
-		if (ch == '\r')
-			usart1_send_char('\n');
-		usart1_send_char(ch);
-	}
+	blink(LED_GREEN);
 }
