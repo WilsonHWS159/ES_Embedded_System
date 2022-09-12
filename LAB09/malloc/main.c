4dac54c5e20f6fc1d006335b7e9f7df8aa0f5038
WilsonHWS159 <n96101189@gs.ncku.edu.tw>
2022-09-12 20:55:09 +0800

    Finish LAB09-1 requirment

---
+++ LAB09/malloc/main.c
@@ -0,0 +1,125 @@
+#include <stdint.h>
+#include <stddef.h>
+#include "blink.h"
+#include "reg.h"
+
+#define HEAP_MAX (65536) // 64KB
+
+const char *reset_msg = "Hello, world!\r\n";
+const char *ore_msg = "Overrun !!!\r\n";
+
+void *_sbrk(int incr) {
+	extern uint8_t _mybss_vma_end;
+	static uint8_t *heap_end = NULL;
+	uint8_t *prev_heap_end;
+
+	if (heap_end == NULL)
+		heap_end = &_mybss_vma_end;
+
+	prev_heap_end = heap_end;
+	if (heap_end + incr > &_mybss_vma_end + HEAP_MAX)
+		return (void *) -1;
+	
+	heap_end += incr;
+	return (void *) prev_heap_end;
+}
+
+void init_usart1(void)
+{
+	//PB6 PB7
+
+	//RCC EN GPIOB
+	SET_BIT(RCC_BASE + RCC_AHB1ENR_OFFSET, GPIO_EN_BIT(GPIO_PORTB));
+
+	//GPIO Configurations
+	SET_BIT(GPIO_BASE(GPIO_PORTB) + GPIOx_MODER_OFFSET, MODERy_1_BIT(6));
+	SET_BIT(GPIO_BASE(GPIO_PORTB) + GPIOx_MODER_OFFSET, MODERy_1_BIT(7));
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTB) + GPIOx_MODER_OFFSET, MODERy_0_BIT(6));
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTB) + GPIOx_MODER_OFFSET, MODERy_0_BIT(7));
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTB) + GPIOx_OTYPER_OFFSET, OTy_BIT(6));
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTB) + GPIOx_OTYPER_OFFSET, OTy_BIT(7));
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTB) + GPIOx_OSPEEDR_OFFSET, OSPEEDRy_1_BIT(6));
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTB) + GPIOx_OSPEEDR_OFFSET, OSPEEDRy_1_BIT(7));
+	SET_BIT(GPIO_BASE(GPIO_PORTB) + GPIOx_OSPEEDR_OFFSET, OSPEEDRy_0_BIT(6));
+	SET_BIT(GPIO_BASE(GPIO_PORTB) + GPIOx_OSPEEDR_OFFSET, OSPEEDRy_0_BIT(7));
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTB) + GPIOx_PUPDR_OFFSET, PUPDRy_1_BIT(6));
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTB) + GPIOx_PUPDR_OFFSET, PUPDRy_1_BIT(7));
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTB) + GPIOx_PUPDR_OFFSET, PUPDRy_0_BIT(6));
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTB) + GPIOx_PUPDR_OFFSET, PUPDRy_0_BIT(7));
+	WRITE_BITS(GPIO_BASE(GPIO_PORTB) + GPIOx_AFRL_OFFSET, AFRLy_3_BIT(6), AFRLy_0_BIT(6), 0x7);
+	WRITE_BITS(GPIO_BASE(GPIO_PORTB) + GPIOx_AFRL_OFFSET, AFRLy_3_BIT(7), AFRLy_0_BIT(7), 0x7);
+	
+
+	//RCC EN USART1
+	SET_BIT(RCC_BASE + RCC_APB2ENR_OFFSET, USART1EN);
+
+	//Baud
+	const unsigned int BAUD = 115200;
+	const unsigned int SYSCLK_MHZ = 168;
+	const double USARTDIV = SYSCLK_MHZ * 1.0e6 / 16 / BAUD;
+
+	const uint32_t DIV_MANTISSA = (uint32_t) USARTDIV;
+	const uint32_t DIV_FRACTION = (uint32_t) (((USARTDIV - DIV_MANTISSA) * 16) + 0.5);
+
+	//USART  Configurations
+	SET_BIT(USART1_BASE + USART_CR1_OFFSET, UE_BIT);
+	WRITE_BITS(USART1_BASE + USART_BRR_OFFSET, DIV_MANTISSA_11_BIT, DIV_MANTISSA_0_BIT, DIV_MANTISSA);
+	WRITE_BITS(USART1_BASE + USART_BRR_OFFSET, DIV_FRACTION_3_BIT, DIV_FRACTION_0_BIT, DIV_FRACTION);
+	SET_BIT(USART1_BASE + USART_CR1_OFFSET, TE_BIT);
+	SET_BIT(USART1_BASE + USART_CR1_OFFSET, RE_BIT);
+	SET_BIT(USART1_BASE + USART_CR1_OFFSET, RXNEIE_BIT);
+	SET_BIT(NVIC_ISER_BASE + NVIC_ISERn_OFFSET(1), 5);
+}
+
+void usart1_send_char(const char ch)
+{
+	for (; !READ_BIT(USART1_BASE + USART_SR_OFFSET, TXE_BIT););
+	WRITE_BITS(USART1_BASE + USART_DR_OFFSET, 0x8, 0x0, ch);
+}
+
+char usart1_receive_char(void)
+{
+	return (char) REG(USART1_BASE + USART_DR_OFFSET);
+}
+
+void usart1_handler()
+{
+	if (READ_BIT(USART1_BASE + USART_SR_OFFSET, ORE_BIT)) {
+		for (char *p = ore_msg; *p != '\0';)
+			usart1_send_char(*p++);
+		blink_count(LED_RED, 1);
+	} else if (READ_BIT(USART1_BASE + USART_SR_OFFSET, RXNE_BIT)) {
+		char ch = usart1_receive_char();
+		if (ch == '\r')
+			usart1_send_char('\n');
+		usart1_send_char(ch);
+		blink_count(LED_BLUE, 1);
+	}
+}
+
+int main(void)
+{
+	init_usart1();
+
+	//send Hello world
+	for (char *p = reset_msg; *p != '\0';)
+		usart1_send_char(*p++);
+
+	char *test_msg = malloc(7 * sizeof(char));
+	if (test_msg != NULL) {
+		test_msg[0] = 'T';
+		test_msg[1] = 'e';
+		test_msg[2] = 's';
+		test_msg[3] = 't';
+		test_msg[4] = '\r';
+		test_msg[5] = '\n';
+		test_msg[6] = '\0';
+
+		for (char *p = test_msg; *p != '\0';)
+			usart1_send_char(*p++);
+
+		free(test_msg);
+	}
+
+	blink(LED_GREEN);
+}
